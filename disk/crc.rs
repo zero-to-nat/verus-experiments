34991714ee5dfdbe230e8822bf4db9a8fc7bba4b
Nickolai Zeldovich <nickolai@csail.mit.edu>
2024-09-12 07:42:22 -0700

    mostly prove bytes_uncorrupted based on hamming crc64 model

--- disk/crc.rs
+++ disk/crc.rs
@@ -58,6 +58,14 @@ verus! {
         Vec::new()
     }
 
+    // This proof function is marked verifier::external_body to assume that all
+    // CRC-64 values are 8 bytes long.
+    #[verifier::external_body]
+    pub proof fn crc64_spec_len(bytes: Seq<u8>)
+        ensures
+            spec_crc64(bytes).len() == 8,
+    {}
+
     // This proof function is marked verifier::external_body to assume that the
     // CRC64 function, as captured by spec_crc64(), correctly achieves the hamming
     // bounds described in spec_crc64_hamming_bound.
@@ -146,6 +154,92 @@ verus! {
         {
             unimplemented!()
         }
+    }
+
+    pub open spec fn disk_reads(disk: Seq<u8>, addrs: Seq<int>) -> Seq<u8> {
+        addrs.map_values(|a: int| disk[a])
+    }
+
+    pub open spec fn all_elements_unique(seq: Seq<int>) -> bool {
+        forall |i: int, j: int| 0 <= i < j < seq.len() ==> seq[i] != seq[j]
+    }
+
+    // Proof TBD, but should be provable.
+    #[verifier::external_body]
+    pub proof fn hamming_disk_reads(disk1: Seq<u8>, disk2: Seq<u8>, addrs: Seq<int>)
+        requires
+            disk1.len() == disk2.len(),
+            all_elements_unique(addrs),
+        ensures
+            hamming(disk_reads(disk1, addrs), disk_reads(disk2, addrs)) <= hamming(disk1, disk2),
+    {
+    }
+
+    pub proof fn byte_xor_xor(a: u8, b: u8)
+        ensures
+            (a^b) ^ a == b,
+    {
+        assert((a^b) ^ a == b) by (bit_vector)
+    }
+
+    pub proof fn list_xor_xor(a: Seq<u8>, b: Seq<u8>)
+        requires
+            a.len() == b.len(),
+        ensures
+            xor(xor(a, b), a) == b,
+        decreases
+            a.len()
+    {
+        if a.len() == 0 {
+            assert(xor(xor(a, b), a) == b)
+        } else {
+            byte_xor_xor(a[0], b[0]);
+            list_xor_xor(a.subrange(1, a.len() as int), b.subrange(1, b.len() as int));
+
+            assert(a == seq![a[0]] + a.subrange(1, a.len() as int));
+            assert(b == seq![b[0]] + b.subrange(1, b.len() as int));
+            assert(xor(seq![a[0]] + a.subrange(1, a.len() as int),
+                       seq![b[0]] + b.subrange(1, b.len() as int)) ==
+                   seq![a[0] ^ b[0]] +
+                   xor(a.subrange(1, a.len() as int), b.subrange(1, b.len() as int)));
+            assert(xor(seq![a[0] ^ b[0]] + xor(a.subrange(1, a.len() as int), b.subrange(1, b.len() as int)),
+                       seq![a[0]] + a.subrange(1, a.len() as int)) ==
+                   seq![(a[0] ^ b[0]) ^ a[0]] +
+                   xor(xor(a.subrange(1, a.len() as int), b.subrange(1, b.len() as int)),
+                       a.subrange(1, a.len() as int)));
+
+            // Q: how to search for verus lemmas?
+        }
+    }
+
+    pub proof fn bytes_uncorrupted(x_c: Seq<u8>, x: Seq<u8>, x_addrs: Seq<int>,
+                                   y_c: Seq<u8>, y: Seq<u8>, y_addrs: Seq<int>,
+                                   disk: Seq<u8>, corrupt: Seq<u8>)
+        requires
+            x_c.len() == x.len(),
+            y == spec_crc64(x),
+            y_c == spec_crc64(x_c),
+
+            all_elements_unique(x_addrs + y_addrs),
+            corrupt.len() == disk.len(),
+
+            x == disk_reads(disk, x_addrs),
+            x_c == disk_reads(xor(disk, corrupt), x_addrs),
+            y == disk_reads(disk, y_addrs),
+            y_c == disk_reads(xor(disk, corrupt), y_addrs),
+            popcnt(corrupt) < spec_crc64_hamming_bound((x+y).len()),
+        ensures
+            x == x_c
+    {
+        crc64_spec_len(x);
+        crc64_spec_len(x_c);
+        hamming_disk_reads(xor(disk, corrupt), disk, x_addrs + y_addrs);
+        list_xor_xor(disk, corrupt);
+        assert(disk_reads(xor(disk, corrupt), x_addrs + y_addrs) ==
+               disk_reads(xor(disk, corrupt), x_addrs) + disk_reads(xor(disk, corrupt), y_addrs));
+        assert(disk_reads(disk, x_addrs + y_addrs) ==
+               disk_reads(disk, x_addrs) + disk_reads(disk, y_addrs));
+        crc64_hamming_bound_valid(x_c, x, y_c, y);
     }
 
     pub fn main() {
