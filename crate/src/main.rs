c53f51c9ccd386a0cffd114126e9034041032ec4
Nickolai Zeldovich <nickolai@csail.mit.edu>
2024-10-10 05:55:08 -0700

    checkpoint misc crate experiments

--- crate/src/main.rs
+++ crate/src/main.rs
@@ -34,11 +34,23 @@ verus! {
     #[inline(never)]
     pub fn test4(
         x: u8,
-        Tracked(i1): Tracked<u8>,
-        Tracked(i2): Tracked<u8>,
-        Tracked(i3): Tracked<u8>,
-        Tracked(i4): Tracked<u8>) -> u8 {
+        Ghost(i1): Ghost<u8>,
+        Ghost(i2): Ghost<u8>,
+        Ghost(i3): Ghost<u8>,
+        Ghost(i4): Ghost<u8>) -> u8 {
             x
+    }
+
+    #[inline(never)]
+    pub fn erasure_test(Ghost(x): Ghost<int>) -> Ghost<int> {
+        let Ghost(mut g) = Ghost(x);
+        for _i in 0..1000000 {
+            let Ghost(n) = Ghost(g + 1);
+            proof {
+                g = n;
+            };
+        };
+        Ghost(g)
     }
 
     #[allow(unused_variables)]
@@ -54,7 +66,13 @@ verus! {
 
         test3();
 
-        let y = test4(123u8, Tracked(0u8), Tracked(0u8), Tracked(0u8), Tracked(0u8));
+        let ghost ga0 = 0u8;
+        let a0 = Ghost(ga0);
+        let a1 = Ghost(1u8);
+
+        let Ghost(x) = erasure_test(Ghost(0int));
+
+        let y = test4(123u8, a0, a0, a0, a1);
         println!("{y}");
 
         // println!("{p1} {p2} {x}");
