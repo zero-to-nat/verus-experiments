6283f4a7b424460f7551386ad9143b4348d4537d
Nickolai Zeldovich <nickolai@csail.mit.edu>
2024-10-11 10:01:44 -0700

    cleanup crate example

--- crate/src/main.rs
+++ crate/src/main.rs
@@ -1,81 +1,29 @@
 use vstd::prelude::*;
-use vstd::invariant::*;
 
 verus! {
 
-    pub struct InvParam {}
-    #[allow(dead_code)]
-    pub struct InvState { x: u64 }
-    pub struct InvPred {}
-
-    impl InvariantPredicate<InvParam, InvState> for InvPred {
-        closed spec fn inv(k: InvParam, v: InvState) -> bool {
-            v.x == 5 || v.x == 10
-        }
+    #[inline(never)]
+    pub fn test1() {
     }
 
     #[inline(never)]
-    pub fn test1(Tracked(i): Tracked<&AtomicInvariant<InvParam, InvState, InvPred>>, Tracked(cr): Tracked<OpenInvariantCredit>) {
-        proof {
-            open_atomic_invariant!(cr => i => inner => {
-                inner.x = 10u64
-            });
-        };
-    }
-
-    #[inline(never)]
-    pub fn test2(Tracked(i): Tracked<&AtomicInvariant<InvParam, InvState, InvPred>>) {
-    }
-
-    #[inline(never)]
-    pub fn test3() {
-    }
-
-    #[inline(never)]
-    pub fn test4(
-        x: u8,
+    pub fn test2(
         Ghost(i1): Ghost<u8>,
         Ghost(i2): Ghost<u8>,
         Ghost(i3): Ghost<u8>,
-        Ghost(i4): Ghost<u8>) -> u8 {
-            x
-    }
-
-    #[inline(never)]
-    pub fn erasure_test(Ghost(x): Ghost<int>) -> Ghost<int> {
-        let Ghost(mut g) = Ghost(x);
-        for _i in 0..1000000 {
-            let Ghost(n) = Ghost(g + 1);
-            proof {
-                g = n;
-            };
-        };
-        Ghost(g)
+        Ghost(i4): Ghost<u8>) {
     }
 
     #[allow(unused_variables)]
     fn main() {
-        let inv_param = InvParam{};
-        let inv_st = InvState{ x: 5 };
-        let tracked i = AtomicInvariant::<_, _, InvPred>::new(inv_param, inv_st, 123int);
+        test1();
 
-        let Tracked(cr) = create_open_invariant_credit();
-        test1(Tracked(&i), Tracked(cr));
-
-        test2(Tracked(&i));
-
-        test3();
-
-        let ghost ga0 = 0u8;
-        let a0 = Ghost(ga0);
+        let a0 = Ghost(0u8);
         let a1 = Ghost(1u8);
+        let a2 = Ghost(1u8);
+        let a3 = Ghost(1u8);
 
-        let Ghost(x) = erasure_test(Ghost(0int));
-
-        let y = test4(123u8, a0, a0, a0, a1);
-        println!("{y}");
-
-        // println!("{p1} {p2} {x}");
+        test2(a0, a1, a2, a3);
     }
 
 } // verus!
