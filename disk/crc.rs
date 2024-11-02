986cb476b0544e1582c7fd5ee9d9535d3e425190
Nickolai Zeldovich <nickolai@csail.mit.edu>
2024-11-01 18:41:14 -0700

    checkpoint

--- disk/crc.rs
+++ disk/crc.rs
@@ -1,11 +1,16 @@
 use vstd::prelude::*;
+use vstd::bytes::*;
 
 mod hamming;
 
 use hamming::*;
 
 verus! {
-    pub closed spec fn spec_crc64(bytes: Seq<u8>) -> Seq<u8>;
+    pub open spec fn spec_crc64_bytes(bytes: Seq<u8>) -> Seq<u8> {
+        spec_u64_to_le_bytes(spec_crc64_u64(bytes))
+    }
+
+    pub closed spec fn spec_crc64_u64(bytes: Seq<u8>) -> u64;
 
     pub open spec fn spec_crc64_hamming_bound(len: nat) -> nat {
         // From https://users.ece.cmu.edu/~koopman/crc/crc64.html as one example.
@@ -32,10 +37,18 @@ verus! {
     #[verifier::external_body]
     pub fn crc64(bytes: &[u8]) -> (out: Vec<u8>)
         ensures
-            out@ == spec_crc64(bytes@),
+            out@ == spec_crc64_bytes(bytes@),
     {
         Vec::new()
     }
+
+    // This proof function is marked verifier::external_body to assume that all
+    // CRC-64 values are 8 bytes long.
+    #[verifier::external_body]
+    pub proof fn crc64_spec_len()
+        ensures
+            forall |bytes| (#[trigger] spec_crc64_bytes(bytes)).len() == 8,
+    {}
 
     // This proof function is marked verifier::external_body to assume that the
     // CRC64 function, as captured by spec_crc64(), correctly achieves the hamming
@@ -43,8 +56,8 @@ verus! {
     #[verifier::external_body]
     pub proof fn crc64_hamming_bound_valid(bytes1: Seq<u8>, bytes2: Seq<u8>, crc1: Seq<u8>, crc2: Seq<u8>)
         requires
-            crc1 == spec_crc64(bytes1),
-            crc2 == spec_crc64(bytes2),
+            crc1 == spec_crc64_bytes(bytes1),
+            crc2 == spec_crc64_bytes(bytes2),
             (bytes1 + crc1).len() == (bytes2 + crc2).len(),
         ensures
             bytes1 == bytes2 ||
@@ -56,8 +69,8 @@ verus! {
                                    disk: Seq<u8>, corrupt: Seq<u8>)
         requires
             x_c.len() == x.len(),
-            y == spec_crc64(x),
-            y_c == spec_crc64(x_c),
+            y == spec_crc64_bytes(x),
+            y_c == spec_crc64_bytes(x_c),
 
             (x_addrs + y_addrs).no_duplicates(),
             corrupt.len() == disk.len(),
@@ -133,6 +146,20 @@ verus! {
         }
 
         #[verifier::external_body]
+        pub fn write_range(&mut self, addr: u64, bytes: &[u8])
+            requires
+                old(self).inv(),
+                addr + bytes@.len() <= old(self)@.len(),
+            ensures
+                self.inv(),
+                self@ == old(self)@.map(|pos: int, pre_byte: u8| if addr <= pos < addr + bytes@.len() { bytes@[pos-addr] } else { pre_byte }),
+                self.corrupt() == old(self).corrupt(),
+                self.corrupt_bits() == old(self).corrupt_bits(),
+        {
+            unimplemented!()
+        }
+
+        #[verifier::external_body]
         pub fn read(&self, addr: u64) -> (res: (u8, Ghost<Seq<u8>>))
             requires
                 self.inv(),
@@ -157,6 +184,20 @@ verus! {
         }
     }
 
+    // This is also unproven in storage_node/src/pmem/pmemspec_t.rs..
+    #[verifier::external_body]
+    pub exec fn crc_equal(crc1: &[u8], crc2: &[u8]) -> bool
+        requires
+            crc1.len() == 8,
+            crc2.len() == 8,
+        returns
+            crc1@ == crc2@
+    {
+        let crc1_u64 = u64_from_le_bytes(crc1);
+        let crc2_u64 = u64_from_le_bytes(crc2);
+        crc1_u64 == crc2_u64
+    }
+
     pub fn main() {
         // assert(popcnt(0) == 0);
         // assert(hamming_byte(0x00, 0x01) == 1);
@@ -174,10 +215,37 @@ verus! {
         assert(v0@[0] == d0@[5]);
 
         let mut d1 = HammingDisk::alloc(128, Ghost(1));
-        d1.write(5, 123);
-        assert(d1@[5] == 123);
-        let (v1, Ghost(mask1)) = d1.read_range(5, 1);
-        assert(v1@[0] == xor(d1@, and(mask1, d1.corrupt()))[5]);
+        let buf = vec![123, 124, 125, 126];
+        let crc = crc64(buf.as_slice());
+        proof {
+            crc64_spec_len();
+        }
+        d1.write_range(10, buf.as_slice());
+        d1.write_range(20, crc.as_slice());
+        assert(buf@ == d1@.subrange(10, 14));
+        assert(crc@ == d1@.subrange(20, 28));
+        let (bufR, Ghost(mask1)) = d1.read_range(10, 4);
+        let (crcR, Ghost(mask2)) = d1.read_range(20, 8);
+
+        assert(bufR@ == xor(d1@, and(mask1, d1.corrupt())).subrange(10, 14));
+        assert(crcR@ == xor(d1@, and(mask2, d1.corrupt())).subrange(20, 28));
+
+        let crc2 = crc64(bufR.as_slice());
+        if crc_equal(crcR.as_slice(), crc2.as_slice()) {
+            proof {
+                let buf_addrs = seq![10, 11, 12, 13];
+                let crc_addrs = seq![20, 21, 22, 23, 24, 25, 26, 27];
+                assert(buf@ == S(d1@)[buf_addrs]);
+                assert(crc@ == S(d1@)[crc_addrs]);
+                assert(bufR@ == S(xor(d1@, d1.corrupt()))[buf_addrs]);
+                assert(crcR@ == S(xor(d1@, d1.corrupt()))[crc_addrs]);
+                bytes_uncorrupted(bufR@, buf@, buf_addrs,
+                                  crcR@, crc@, crc_addrs,
+                                  d1@, d1.corrupt());
+            }
+            assert(bufR@ == buf@);
+        }
+
         // proof {
         //     popcnt_and(mask1, d1.corrupt());
         //     xor_zeroes(d1@, and(mask1, d1.corrupt()));
