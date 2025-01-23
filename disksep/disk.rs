331f6f82d29f58e719fe683c8026bb38fad8c0a2
Nickolai Zeldovich <nickolai@csail.mit.edu>
2025-01-22 16:39:21 -0800

    add prophecy-style crash spec to low-level disk model

--- disksep/disk.rs
+++ disksep/disk.rs
@@ -5,11 +5,21 @@ verus! {
 
     pub struct Disk {
         store: Vec<u8>,
+        persist: Ghost<Seq<u8>>,
     }
 
     pub open spec fn update_bytes(s: Seq<u8>, addr: int, bytes: Seq<u8>) -> Seq<u8>
     {
         Seq::new(s.len(), |i: int| if addr <= i < addr + bytes.len() { bytes[i - addr] } else { s[i] })
+    }
+
+    pub open spec fn can_result_from_write(post: Seq<u8>, pre: Seq<u8>, addr: int, bytes: Seq<u8>) -> bool
+    {
+        &&& post.len() == pre.len()
+        &&& forall |i| 0 <= i < pre.len() ==> {
+            ||| post[i] == pre[i]
+            ||| addr <= i < addr + bytes.len() && post[i] == bytes[i-addr]
+        }
     }
 
     #[verifier::external_body]
@@ -26,12 +36,17 @@ verus! {
     impl Disk {
         pub closed spec fn inv(&self) -> bool
         {
-            true
+            self.persist@.len() == self.store@.len()
         }
 
         pub closed spec fn view(&self) -> Seq<u8>
         {
             self.store@
+        }
+
+        pub closed spec fn persist(&self) -> Seq<u8>
+        {
+            self.persist@
         }
 
         pub fn read_one(&self, a: usize) -> (result: u8)
@@ -50,10 +65,18 @@ verus! {
                 a < old(self)@.len()
             ensures
                 self.inv(),
-                self@ == old(self)@.update(a as int, v)
+                self@ == old(self)@.update(a as int, v),
+                can_result_from_write(self.persist(), old(self).persist(), a as int, seq![v]),
         {
             // XXX Where is Vec::set() coming from?
             self.store.set(a, v);
+
+            proof {
+                let persist_prophecy: bool = arbitrary();
+                if persist_prophecy {
+                    *self.persist.borrow_mut() = self.persist@.update(a as int, v);
+                }
+            }
         }
 
         #[verifier::external_body]
@@ -75,19 +98,36 @@ verus! {
             ensures
                 self.inv(),
                 self@ == update_bytes(old(self)@, a as int, v@),
+                can_result_from_write(self.persist(), old(self).persist(), a as int, v@),
         {
             self.store.splice(a..a+v.len(), v.iter().cloned());
+            self.persist = arbitrary();
+        }
+
+        pub fn flush(&mut self)
+            requires
+                old(self).inv(),
+            ensures
+                self.inv(),
+                self@ == old(self)@,
+                self.persist() == self@,
+        {
+            proof {
+                *self.persist.borrow_mut() = self.store@;
+            }
         }
 
         pub fn new(n: usize) -> (result: Disk)
             ensures
                 result.inv(),
                 result@ =~= Seq::<u8>::new(n as nat, |i: int| 0),
+                result.persist() == result@,
         {
             let mut store = Vec::new();
             store.resize(n, 0);
             Disk{
-                store: store
+                store: store,
+                persist: Ghost(store@),
             }
         }
     }
