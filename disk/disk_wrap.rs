3baa39ff92090c155891050713044ff3ee542a59
Nickolai Zeldovich <nickolai@csail.mit.edu>
2025-01-24 11:59:27 -0800

    rename Op to Lin, since it's a Linearizer

--- disk/disk_wrap.rs
+++ disk/disk_wrap.rs
@@ -4,10 +4,14 @@ use super::seq_view::*;
 use super::logatom::*;
 
 verus! {
+    pub struct DiskResources {
+        pub latest: SeqAuth<u8>,
+        pub persist: SeqAuth<u8>,
+    }
+
     pub struct DiskWrap {
         d: Disk,
-        a: Tracked<SeqAuth<u8>>,
-        pa: Tracked<SeqAuth<u8>>,
+        r: Tracked<DiskResources>,
     }
 
     pub struct WriteOp {
@@ -34,25 +38,45 @@ verus! {
         }
     }
 
+    pub struct FlushOp {
+        pub id: int,
+        pub persist_id: int,
+    }
+
+    impl ReadOperation for FlushOp {
+        type Resource = DiskResources;
+        type ExecResult = ();
+
+        open spec fn requires(self, r: Self::Resource, e: ()) -> bool {
+            &&& r.latest.inv()
+            &&& r.latest.id() == self.id
+
+            &&& r.persist.inv()
+            &&& r.persist.id() == self.persist_id
+
+            &&& r.latest@ == r.persist@
+        }
+    }
+
     impl DiskWrap {
         pub closed spec fn inv(self) -> bool
         {
             &&& self.d.inv()
-            &&& self.a@.inv()
-            &&& self.pa@.inv()
-            &&& self.d@ =~= self.a@@
-            &&& self.d.persist() =~= self.pa@@
-            &&& self.pa@@.len() == self.a@@.len()
+            &&& self.r@.latest.inv()
+            &&& self.r@.persist.inv()
+            &&& self.r@.latest@ =~= self.d@
+            &&& self.r@.persist@ =~= self.d.persist()
+            &&& self.r@.latest@.len() == self.r@.persist@.len()
         }
 
         pub closed spec fn id(self) -> int
         {
-            self.a@.id()
+            self.r@.latest.id()
         }
 
         pub closed spec fn persist_id(self) -> int
         {
-            self.pa@.id()
+            self.r@.persist.id()
         }
 
         pub fn read(&self, a: usize, len: usize, Tracked(perm): Tracked<&SeqFrac<u8>>) -> (result: Vec<u8>)
@@ -65,22 +89,22 @@ verus! {
                 result@ =~= perm@,
         {
             proof {
-                perm.agree(self.a.borrow());
+                perm.agree(&self.r.borrow().latest);
             }
             self.d.read(a, len)
         }
 
-        pub fn write<Op>(&mut self, a: usize, v: &[u8],
-                         Tracked(perm): Tracked<&mut SeqFrac<u8>>,
-                         Tracked(p_op): Tracked<Op>) -> (result: Tracked<Op::ApplyResult>)
+        pub fn write<Lin>(&mut self, a: usize, v: &[u8],
+                          Tracked(perm): Tracked<&mut SeqFrac<u8>>,
+                          Tracked(p_lin): Tracked<Lin>) -> (result: Tracked<Lin::ApplyResult>)
             where
-                Op: MutLinearizer<WriteOp>,
+                Lin: MutLinearizer<WriteOp>,
             requires
                 old(self).inv(),
                 old(perm).valid(old(self).id()),
                 old(perm).off() == a,
                 old(perm)@.len() == v@.len(),
-                p_op.pre(WriteOp{ persist_id: old(self).persist_id(), addr: a, data: v@ }),
+                p_lin.pre(WriteOp{ persist_id: old(self).persist_id(), addr: a, data: v@ }),
             ensures
                 self.inv(),
                 self.id() == old(self).id(),
@@ -88,15 +112,30 @@ verus! {
                 perm.valid(self.id()),
                 perm.off() == old(perm).off(),
                 perm@ =~= v@,
-                p_op.post(WriteOp{ persist_id: old(self).persist_id(), addr: a, data: v@ }, (), result@),
+                p_lin.post(WriteOp{ persist_id: old(self).persist_id(), addr: a, data: v@ }, (), result@),
         {
             proof {
-                perm.agree(self.a.borrow());
+                perm.agree(&self.r.borrow().latest);
             }
             self.d.write(a, v);
             Tracked({
-                perm.update(self.a.borrow_mut(), v@);
-                p_op.apply(WriteOp{ persist_id: old(self).persist_id(), addr: a, data: v@ }, self.d.persist().subrange(a as int, a+v.len()), self.pa.borrow_mut(), &())
+                perm.update(&mut self.r.borrow_mut().latest, v@);
+                p_lin.apply(WriteOp{ persist_id: old(self).persist_id(), addr: a, data: v@ }, self.d.persist().subrange(a as int, a+v.len()), &mut self.r.borrow_mut().persist, &())
+            })
+        }
+
+        pub fn flush<Lin>(&self, Tracked(p_lin): Tracked<Lin>) -> (result: Tracked<Lin::ApplyResult>)
+            where
+                Lin: ReadLinearizer<FlushOp>,
+            requires
+                self.inv(),
+                p_lin.pre(FlushOp{ id: self.id(), persist_id: self.persist_id() }),
+            ensures
+                p_lin.post(FlushOp{ id: self.id(), persist_id: self.persist_id() }, (), result@),
+        {
+            self.d.flush();
+            Tracked({
+                p_lin.apply(FlushOp{ id: self.id(), persist_id: self.persist_id() }, self.r.borrow(), &())
             })
         }
 
@@ -118,8 +157,10 @@ verus! {
             let tracked (par, pfr) = SeqAuth::<u8>::new(d.persist());
             let dw = DiskWrap{
                 d: d,
-                a: Tracked(ar),
-                pa: Tracked(par),
+                r: Tracked(DiskResources{
+                    latest: ar,
+                    persist: par,
+                }),
             };
             (dw, Tracked(fr), Tracked(pfr))
         }
