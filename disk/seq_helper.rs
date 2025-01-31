2b59a36211885a3782c6580993a3fdb39d05dffa
Nickolai Zeldovich <nickolai@csail.mit.edu>
2025-01-31 14:00:36 -0800

    start adding a transactional-write interface

--- disk/seq_helper.rs
+++ disk/seq_helper.rs
@@ -5,7 +5,25 @@ verus! {
         Seq::new(s.len(), |i: int| if off <= i < off + v.len() { v[i - off] } else { s[i] })
     }
 
-    pub open spec fn seq_to_map<V>(s: Seq<V>, off: int) -> Map<int, V> {
-        Map::new(|i| off <= i < off + s.len(), |i: int| s[i-off])
+    pub open spec fn update_seq_map_int<V>(s: Seq<V>, m: Map<int, V>) -> Seq<V>
+        recommends
+            forall |i| m.contains_key(i) ==> 0 <= i < s.len()
+    {
+        Seq::new(s.len(), |i: int| if m.contains_key(i) { m[i] } else { s[i] })
+    }
+
+    pub open spec fn update_seq_map_usize<V>(s: Seq<V>, m: Map<usize, V>) -> Seq<V>
+        recommends
+            forall |i| m.contains_key(i) ==> 0 <= i < s.len()
+    {
+        Seq::new(s.len(), |i: int| if m.contains_key(i as usize) { m[i as usize] } else { s[i] })
+    }
+
+    pub open spec fn seq_to_map_int<V>(s: Seq<V>, off: int) -> Map<int, V> {
+        Map::new(|i: int| off <= i < off + s.len(), |i: int| s[i-off])
+    }
+
+    pub open spec fn seq_to_map_usize<V>(s: Seq<V>, off: usize) -> Map<usize, V> {
+        Map::new(|i: usize| off <= i < off + s.len(), |i: usize| s[i-off])
     }
 }
--- disk/vecdisk.rs
+++ disk/vecdisk.rs
@@ -1,5 +1,6 @@
 use vstd::prelude::*;
 use super::seq_helper::*;
+use std::collections::HashMap;
 
 verus! {
     broadcast use vstd::std_specs::hash::group_hash_axioms;
@@ -18,6 +19,15 @@ verus! {
         }
     }
 
+    pub open spec fn can_result_from_write_map(post: Seq<u8>, pre: Seq<u8>, writeset: Map<usize, u8>) -> bool
+    {
+        &&& post.len() == pre.len()
+        &&& forall |i| 0 <= i < pre.len() ==> {
+            ||| post[i] == pre[i]
+            ||| writeset.contains_key(i as usize) && post[i] == writeset[i as usize]
+        }
+    }
+
     #[verifier::external_body]
     pub fn copy_from_slice(bytes: &[u8]) -> (out: Vec<u8>)
         ensures
@@ -29,20 +39,80 @@ verus! {
         buffer
     }
 
+    pub struct WriteSet {
+        pub writeset: HashMap<usize, u8>,
+    }
+
+    impl WriteSet {
+        pub open spec fn valid(self, disklen: nat) -> bool {
+            forall |i: usize| self.writeset@.contains_key(i) ==> i < disklen
+        }
+
+        pub open spec fn view(self) -> Map<usize, u8> {
+            self.writeset@
+        }
+
+        pub fn write(self: &mut Self, a: usize, v: &[u8], Ghost(disklen): Ghost<usize>)
+            requires
+                old(self).valid(disklen as nat),
+                v@.len() > 0 ==> a + v@.len() <= disklen,
+            ensures
+                self.valid(disklen as nat),
+                self@ =~= old(self)@.union_prefer_right(seq_to_map_usize(v@, a)),
+        {
+            broadcast use vstd::map::group_map_axioms;
+
+            for i in 0..v.len()
+                invariant
+                    old(self).valid(disklen as nat),
+                    self.valid(disklen as nat),
+                    v@.len() > 0 ==> a + v@.len() <= disklen,
+                    self.view() =~= old(self)@.union_prefer_right(seq_to_map_usize(v@.subrange(0, i as int), a)),
+            {
+                let ghost tv = self@;
+                assert(self@ =~= tv);
+                assert forall |j| self.writeset@.contains_key(j as usize) implies self.writeset@[j as usize] == tv[j] by {}
+
+                let ghost w = self.writeset@;
+                self.writeset.insert(a+i, v[i]);
+                let ghost y = self.writeset@;
+                assert(y == w.insert((a+i) as usize, v[i as int]));
+
+                assert forall |j: int| #[trigger] y.contains_key(j as usize) && j as usize != (a+i) as usize implies y[j as usize] == w[j as usize] by {
+                    vstd::map::axiom_map_insert_different(w, (a+i) as usize, j as usize, v[i as int]);
+                }
+
+                assert forall |j| self.writeset@.contains_key(j as usize) && j != a+i implies self.writeset@[j as usize] == tv[j] by {}
+                assert(self.writeset@.contains_key((a+i) as usize));
+                assert(self.writeset@[(a+i) as usize] == v[i as int]);
+                assert(self@[(a+i) as usize] == v[i as int]);
+                assert(self@ =~= tv.insert((a+i) as usize, v[i as int]));
+            }
+        }
+    }
+
     impl Disk {
-        pub closed spec fn inv(&self) -> bool
+        pub closed spec fn inv(self) -> bool
         {
             self.persist@.len() == self.store@.len()
         }
 
-        pub closed spec fn view(&self) -> Seq<u8>
+        pub closed spec fn view(self) -> Seq<u8>
         {
             self.store@
         }
 
-        pub closed spec fn persist(&self) -> Seq<u8>
+        pub closed spec fn persist(self) -> Seq<u8>
         {
             self.persist@
+        }
+
+        pub proof fn disklen(self) -> (result: usize)
+            ensures
+                result == self@.len(),
+        {
+            vstd::std_specs::vec::axiom_spec_len(&self.store);
+            self@.len() as usize
         }
 
         pub fn read_one(&self, a: usize) -> (result: u8)
@@ -124,6 +194,42 @@ verus! {
                 store: store,
                 persist: Ghost(store@),
             }
+        }
+
+        pub fn begin(&self) -> (result: WriteSet)
+            requires
+                self.inv(),
+            ensures
+                result.valid(self@.len()),
+                result.writeset.len() == 0,
+        {
+            WriteSet{
+                writeset: HashMap::new(),
+            }
+        }
+
+        #[verifier::external_body]
+        pub fn commit(&mut self, ws: WriteSet)
+            requires
+                old(self).inv(),
+                ws.valid(old(self)@.len()),
+            ensures
+                self.inv(),
+                self@ == update_seq_map_usize(old(self)@, ws@),
+                can_result_from_write_map(self.persist(), old(self).persist(), ws@),
+        {
+            for (&a, &v) in ws.writeset.iter() {
+                self.store.set(a, v);
+
+                proof {
+                    let persist_prophecy: bool = arbitrary();
+                    if persist_prophecy {
+                        *self.persist.borrow_mut() = self.persist@.update(a as int, v);
+                    }
+                }
+            }
+
+            // XXX prove later
         }
     }
 }
